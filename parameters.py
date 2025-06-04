016e2d0d8cd4551e88eb182cdbf982f816260cc5
MDR <119791309+MohammadAlomar8@users.noreply.github.com>
2025-06-04 20:52:32 +0300

    extract the useful projects data

--- parameters.py
+++ parameters.py
@@ -1,4 +1,13 @@
 db_name = 'apache_jira_data'
 connection_link = 'mongodb://localhost:27017/'
-collection_name = 'issues'
+collection_issues = 'issues'
+collection_users = 'users'
+collection_projects = 'projects'
 data_out_folder = r"C:\Users\hp\Desktop\Module-3-Task-assigning\data\data_output"
+min_assignees = 5
+min_issues_per_assignee = 80
+
+# Parameters for running the pipeline
+num_assignees = 5
+all_assignees = [5, 10, 15, 20]
+num_topics = [4, 6, 8, 10, 12, 14, 16, 18, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170]
