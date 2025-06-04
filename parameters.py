cb5c93a9fe693baf7f3a39659b23a7b33f89ef5e
MDR <119791309+MohammadAlomar8@users.noreply.github.com>
2025-06-04 17:43:45 +0300

    get the data using mongoDB & mongosh

---
+++ parameters.py
@@ -0,0 +1,4 @@
+db_name = 'apache_jira_data'
+connection_link = 'mongodb://localhost:27017/'
+collection_name = 'issues'
+data_out_folder = r"C:\Users\hp\Desktop\Module-3-Task-assigning\data\data_output"
