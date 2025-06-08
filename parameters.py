aa213c2b5c0962ba70aa5806d000528891e8f6fe
MDR <119791309+MohammadAlomar8@users.noreply.github.com>
2025-06-08 14:30:53 +0300

    get the best topic with its accuracy

--- parameters.py
+++ parameters.py
@@ -6,8 +6,10 @@ collection_projects = 'projects'
 data_out_folder = r"C:\Users\hp\Desktop\Module-3-Task-assigning\data\data_output"
 trainable_data_folder = r"C:\Users\hp\Desktop\Module-3-Task-assigning\data\data_output\step3_2_processed_data"
 min_assignees = 5
+result_folder = r"results"
 min_issues_per_assignee = 80
 # Parameters for running the pipeline
 num_assignees = 5
 all_assignees = [5, 10, 15, 20]
 num_topics = [4, 6, 8, 10, 12, 14, 16, 18, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170]
+projects = ["AMBARI", "ARROW", "CASSANDRA", "CB", "DATALAB", "FLINK", "GEODE", "HDDS", "IGNITE", "IMPALA", "MESOS", "OAK"]
